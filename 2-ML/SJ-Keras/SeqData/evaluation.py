d0a104fe277a8d6b33687a43fbc20d0c274f3f80
slowmoyang <slowmoyang@gmail.com>
2017-11-22 17:54:24 +0900

    add data loader, train, eval function for sequential data

---
+++ 2-ML/SJ-Keras/SeqData/evaluation.py
@@ -0,0 +1,139 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import sys
+import os.path
+
+import argparse
+from datetime import datetime
+from tqdm import tqdm
+#import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
+
+#import tensorflow as tf
+
+import keras
+from keras.models import load_model
+from keras.utils import multi_gpu_model 
+
+#import keras.backend as K
+
+from models.layer import add_an_sigmoid_layer
+#from pipeline import SeqDataLoader
+from pipeline import DataLoader
+
+sys.path.append("..")
+from custom_losses import binary_cross_entropy_with_logits
+from custom_metrics import accuracy_with_logits
+from meters import ROCMeter, OutHist
+from utils import (
+    get_log_dir,
+    get_saved_model_paths,
+    Logger
+)
+
+
+
+def evaluate(saved_model_path,
+             step,
+             log_dir):
+
+    logger = Logger(log_dir.path, "READ")
+
+    model_logit = load_model(
+        filepath=saved_model_path,
+        custom_objects={
+            "binary_cross_entropy_with_logits": binary_cross_entropy_with_logits,
+            "accuracy_with_logits": accuracy_with_logits,
+        },
+    )
+    model = add_an_sigmoid_layer(model_logit, "prediction")
+
+    out_hist = OutHist(
+        dpath=log_dir.output_histogram.path,
+        step=step,
+        dname_list=["train", "test_dijet", "test_zjet"])
+
+    # on training data
+    train_data_loader = DataLoader(
+        path=logger["train_data"],
+        maxlen=logger["maxlen"],
+        batch_size=1000,
+        cyclic=False)
+
+    for x_daus, x_glob, y in train_data_loader:
+        preds = model.predict_on_batch([x_daus, x_glob])
+        out_hist.fill(dname="train", labels=y, preds=preds)
+
+    # Test on dijet dataset
+    test_dijet_loader = DataLoader(
+        path=logger["val_dijet_data"],
+        maxlen=logger["maxlen"],
+        batch_size=1000,
+        cyclic=False)
+
+    roc_dijet = ROCMeter(
+        dpath=log_dir.roc.path,
+        step=step,
+        title="Test on Dijet",
+        prefix="dijet_"
+    )
+
+    for x_daus, x_glob, y in test_dijet_loader:
+        preds = model.predict_on_batch([x_daus, x_glob])
+
+        roc_dijet.append(labels=y, preds=preds)
+        out_hist.fill(dname="test_dijet", labels=y, preds=preds)
+
+    roc_dijet.finish()
+
+    # Test on Z+jet dataset
+    test_zjet_loader = DataLoader(
+        path=logger["val_zjet_data"],
+        maxlen=logger["maxlen"],
+        batch_size=1000,
+        cyclic=False)
+
+    roc_zjet = ROCMeter(
+        dpath=log_dir.roc.path,
+        step=step,
+        title="Test on Z+jet",
+        prefix="zjet_"
+    )
+
+    for x_daus, x_glob, y in test_zjet_loader:
+        preds = model.predict_on_batch([x_daus, x_glob])
+        roc_zjet.append(labels=y, preds=preds)
+        out_hist.fill("test_zjet", labels=y, preds=preds)
+
+    roc_zjet.finish()
+
+    out_hist.finish()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+	    '--log_dir', type=str, required=True,
+    	help='the directory path of dataset')
+
+    args = parser.parse_args()
+
+    log_dir = get_log_dir(path=args.log_dir, creation=False)
+
+
+    path_and_step = get_saved_model_paths(log_dir.saved_models.path)
+    for i, (saved_model_path, step) in enumerate(path_and_step):
+        print("\n\n\n[{i}/{total}]: {path}".format(
+            i=i, total=len(path_and_step), path=saved_model_path))
+        evaluate(
+            saved_model_path,
+            step,
+            log_dir)
+
+
+if __name__ == "__main__":
+    main()
